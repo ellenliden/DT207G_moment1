f6cda439aa2e4d5c2688b3f81ec5ade3e0c525bd
Ellen <lidenellen@gmail.com>
2025-08-13 16:55:53 +0200

    Webbapplikation med Express och EJS. Lagt till navbar med styling.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express');
+//Starta upp applikationen
+const express = require("express");
 const app = express();
+const port = 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
+// EJS
+app.set("view engine", "ejs");
+
+// Statiska filer
+app.use(express.static("public"));
+
+// Route
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
+// Starta upp server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
