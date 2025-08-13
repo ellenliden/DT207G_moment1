64d428759c8ab708da7955c35265f29a047f6f1f
Ellen <lidenellen@gmail.com>
2025-08-13 17:00:59 +0200

    lagt till undersidor, uppdaterat route i server.js för dessa samt uppdaterat menyn på index.html.

--- server.js
+++ server.js
@@ -9,9 +9,24 @@ app.set("view engine", "ejs");
 // Statiska filer
 app.use(express.static("public"));
 
-// Route
+// Route för startsidan
 app.get("/", function (req, res) {
   res.render("index");
+});
+
+// Route för lägga till kurser
+app.get("/add-course", function (req, res) {
+  res.render("add-course");
+});
+
+//Route för visa kurser
+app.get("/courses", function (req, res) {
+  res.render("courses");
+});
+
+//Route för om oss
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 // Starta upp server
